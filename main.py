ac886b027872224db7fd3a43c071ff2bd2e27157
ehmatthes <titanwayne@iMac-2.local>
2024-07-08 16:02:43 +0800

    1

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+@app.get("/items/{item_id}")
+async def get_item(item_id: int, q: str = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
